076efb23108dc83d51bb550c8228d55a81c4f112
CemTE <cem.tercanoglu@th-koeln.de>
2021-07-29 11:30:24 +0200

    feat form

---
+++ pages/form.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main text-center flex flex-col items-center justify-center">
+    <h1 class="title">Form</h1>
+    <h2 class="subtitle">Test</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    }
+  },
+}
+</script>
