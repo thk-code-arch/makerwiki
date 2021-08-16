c4011b976213804f93b8dea01758ae8976697cb5
CemTe <cem.tercanoglu@th-koeln.de>
2021-08-16 10:42:19 +0200

    Styles 2.0

--- formulate.config.js
+++ formulate.config.js
@@ -10,11 +10,22 @@ export default {
     body:'bg-purple-300',
     outer: 'mb-4 m-5 mt-5 my-5 inline-grid',
     input(context) {
+     // console.log(context)
+     
       switch (context.classification) {
+        
         case "button":
-          return "bg-retro-purple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-purple hover:border-black rounded";
+          return "bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded";
+        case "select":
+            return "w-auto bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded";
+        case "box":
+            return "w-auto bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded";
+        case "text":
+              return "border border-gray-400 w-auto bg rounded px-2 py-2 mx-1 mb-1 leading-none focus:border-green-500 outline-none border-box  text-black flex.auto ease-linear transform hover:scale-105 transition duration-500 bg-retro-blue ";
+
         default:
-          return "border border-gray-400 bg rounded px-3 py-2 leading-none focus:border-green-500 outline-none border-box w-full mb-1 text-black flex.auto ease-linear transform hover:scale-105 transition duration-500 bg-retro-blue mx-2";
+          console.log("hab dich",context)
+          return "border border-gray-400 w-auto bg rounded px-2 py-2 mx-1 mb-1 leading-none focus:border-green-500 outline-none border-box  text-black flex.auto ease-linear transform hover:scale-105 transition duration-500 bg-retro-blue ";
       }
     },
     label: 'font-medium text-sm h-6',
