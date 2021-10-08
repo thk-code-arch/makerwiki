5e92c16b93cc8edf5acd3cb1fc7fe2122c6abd0a
CemTe <cem.tercanoglu@th-koeln.de>
2021-10-08 09:34:54 +0200

    Update

---
+++ components/Categories.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <h2 class="subtitle">Kategorie</h2>
+    <FormulateInput type="group" name="kategorie" :repeatable="false">
+      <div class="flex items-center ">
+        <FormulateInput
+          type="select"
+          name="gruppe"
+          label="Gruppe"
+          :options="kategorie.gruppe"
+          v-model="materialData.kategorie.gruppe"
+        />
+        <button
+          class="bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded"
+        >
+          +
+        </button>
+        <FormulateInput
+          type="select"
+          name="unterGruppe"
+          label="Untergruppe"
+          :options="kategorie.unterGruppe"
+          v-model="materialData.kategorie.unterGruppe"
+        />
+        <button
+          class="bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded"
+        >
+          +
+        </button>
+        <FormulateInput
+          type="select"
+          name="art"
+          label="Art"
+          :options="kategorie.art"
+          v-model="materialData.kategorie.art"
+        />
+        <button
+          class="bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded"
+        >
+          +
+        </button>
+      </div>
+    </FormulateInput>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Categories',
+  props: ['categoryData'],
+  data() {
+    return {
+      materialData: {
+        kategorie: { gruppe: '', unterGruppe: '', art: '' }
+      }
+    }
+  },
+  computed: {
+    kategorie() {
+      const kat = {
+        gruppe: this.categoryData.map(g => {
+          return {
+            label: g.label,
+            value: g.value
+          }
+        }),
+        unterGruppe: this.materialData.kategorie.gruppe
+          ? this.categoryData.find(x => x.value === this.materialData.kategorie.gruppe)?.untergruppe
+          : '',
+
+        art: this.materialData.kategorie.unterGruppe
+          ? this.categoryData
+              .find(x => x.value === this.materialData.kategorie.gruppe)
+              .untergruppe.find(x => x.value === this.materialData.kategorie.unterGruppe)?.art
+          : ''
+      }
+
+      console.log('hnorray', kat.unterGruppe.length)
+      return kat
+    }
+  }
+}
+</script>
--- pages/form.vue
+++ pages/form.vue
@@ -12,47 +12,7 @@
         v-model="materialData.bilder"
       />
       <FormulateInput type="text" name="ueberschrift" label="Überschrift" v-model="materialData.ueberschrift" />
-      <h2 class="subtitle">Kategorie</h2>
-      <FormulateInput type="group" name="kategorie" :repeatable="false">
-        <div class="flex items-center ">
-          <FormulateInput
-            type="select"
-            name="gruppe"
-            label="Gruppe"
-            :options="kategorie.gruppe"
-            v-model="materialData.kategorie.gruppe"
-          />
-          <button
-            class="bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded"
-          >
-            +
-          </button>
-          <FormulateInput
-            type="select"
-            name="unterGruppe"
-            label="Untergruppe"
-            :options="kategorie.unterGruppe"
-            v-model="materialData.kategorie.unterGruppe"
-          />
-          <button
-            class="bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded"
-          >
-            +
-          </button>
-          <FormulateInput
-            type="select"
-            name="art"
-            label="Art"
-            :options="kategorie.art"
-            v-model="materialData.kategorie.art"
-          />
-          <button
-            class="bg-retro-mediumpurple hover:bg-retro-black text-white font-bold py-2 px-4 border-b-4 border-retro-Deeppurple hover:border-black rounded"
-          >
-            +
-          </button>
-        </div>
-      </FormulateInput>
+      <Categories :categoryData="categoryData" />
       <FormulateInput type="group" name="mechanischeEigenschaften" :repeatable="false">
         <h2 class="subtitle">Mechanische Eigenschaften</h2>
         <FormulateInput type="group" name="dichte" :repeatable="false">
@@ -313,7 +273,11 @@
 </template>
 
 <script>
+import Categories from '~/components/Categories'
 export default {
+  components: {
+    Categories
+  },
   methods: {
     async createMaterial() {
       console.log('created')
@@ -327,30 +291,6 @@ export default {
   },
   mounted() {
     console.log(this.kategorie)
-  },
-  computed: {
-    kategorie() {
-      const kat = {
-        gruppe: this.categoryData.map(g => {
-          return {
-            label: g.label,
-            value: g.value
-          }
-        }),
-        unterGruppe: this.materialData.kategorie.gruppe
-          ? this.categoryData.find(x => x.value === this.materialData.kategorie.gruppe)?.untergruppe
-          : '',
-
-        art: this.materialData.kategorie.unterGruppe
-          ? this.categoryData
-              .find(x => x.value === this.materialData.kategorie.gruppe)
-              .untergruppe.find(x => x.value === this.materialData.kategorie.unterGruppe)?.art
-          : ''
-      }
-
-      console.log('hnorray', kat.unterGruppe.length)
-      return kat
-    }
   },
 
   data() {
