00127b5a98d6ce107fcac285ece6d573e9dae374
CemTe <cem.tercanoglu@th-koeln.de>
2021-11-11 11:34:31 +0100

    Modal

---
+++ components/Popup.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-white absolute w-full z-10 top-20 mx-auto p-5 border shadow-lg rounded-md">
+    hello
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/material/index.vue
+++ pages/material/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <main v-if="materialPosts" class="main">
+  <main v-if="materialPosts" class="main ">
+    <Popup v-show="showPopup" />
     <h1 class="title text-left">materials</h1>
     <div class=" flex flex-wrap">
-      <div v-for="(materialPost, index) in materialPosts" :key="index" class="text-left flex w-1/3 py-4 rounded-lg">
+      <div
+        v-for="(materialPost, index) in materialPosts"
+        :key="index"
+        class="text-left flex lg:w-1/3 p-4 rounded-lg w-full"
+      >
         <nuxt-link
           class="max-w-5xl mx-auto px-4 py-2 rounded border border-solid border-primary"
           :to="`material/${materialPost.id}`"
@@ -19,6 +24,7 @@
   </main>
 </template>
 <script>
+import Popup from '~/components/Popup.vue'
 export default {
   async asyncData({ $axios }) {
     const materialData = await $axios.$get('api/materials')
@@ -28,6 +34,12 @@ export default {
       materialPosts.push({ id: material._id, material: JSON.parse(material.materialData) })
     }
     return { materialPosts }
+  },
+  components: { Popup },
+  data() {
+    return {
+      showPopup: true
+    }
   }
 }
 </script>
