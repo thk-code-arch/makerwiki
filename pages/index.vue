d5f59156c26b50c3f081e55205f4dc40072f481d
therealstein <therealstein@tutanota.com>
2021-11-24 14:48:39 +0000

    settings

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,6 @@
 export default {
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
     }
   }
 }
---
+++ pages/login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="main text-center flex flex-col items-center justify-center">
+      <FormulateForm
+    class="login-form"
+    v-model="formValues"
+  >
+    <h2 class="subtitle">Register</h2>
+    <p>
+      Please register with Invite Code.
+    </p>
+    <FormulateInput
+      name="name"
+      type="text"
+      label="Your name"
+      placeholder="Your name"
+      validation="required"
+    />
+    <FormulateInput
+      name="email"
+      type="email"
+      label="Email address"
+      placeholder="Email address"
+      validation="required|email"
+    />
+    <div class="double-wide">
+      <FormulateInput
+        name="password"
+        type="password"
+        label="Password"
+        placeholder="Your password"
+        validation="required"
+      />
+      <FormulateInput
+        name="password_confirm"
+        type="password"
+        label="Confirm your password"
+        placeholder="Confirm password"
+        validation="required|confirm"
+        validation-name="Confirmation"
+      />
+    </div>
+    <FormulateInput
+      type="submit"
+      label="Register"
+    />
+    <pre
+      class="code"
+      v-text="formValues"
+    />
+  </FormulateForm>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+    }
+  }
+}
+</script>
