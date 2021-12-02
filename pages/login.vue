8b7b4e06a1abebaceb02914b405102c9027a021c
therealstein <therealstein@tutanota.com>
2021-12-02 18:15:38 +0000

    settings

--- pages/login.vue
+++ pages/login.vue
@@ -1,36 +1,32 @@
 <template>
   <div class="main text-center flex flex-col items-center justify-center">
     <component v-bind:is="currentTabComponent"></component>
-        <div v-for="tab in tabs" v-bind:key="tab">
-          <button
-            v-if="currentTab != tab.component"
-            v-on:click="currentTab = tab.component"
-            class="p-4 white"
-          >
-            {{ tab.name }}
-          </button>
-        </div>
+    <div v-for="tab in tabs" v-bind:key="tab">
+      <button v-if="currentTab != tab.component" v-on:click="currentTab = tab.component" class="p-4 white">
+        {{ tab.name }}
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-import SignIn from "~/components/auth/Signin"
-import Register from "~/components/auth/register"
+import SignIn from '~/components/auth/Signin'
+import Register from '~/components/auth/register'
 export default {
-  components:{SignIn, Register},
+  components: { SignIn, Register },
   data() {
     return {
-      currentTab: "SignIn",
+      currentTab: 'SignIn',
       tabs: [
-        { name: "Register", component: "Register" },
-        { name: "Login", component: "SignIn" },
+        { name: 'Register', component: 'Register' },
+        { name: 'Login', component: 'SignIn' },
       ],
     }
-    },
-    
+  },
+
   computed: {
     currentTabComponent: function () {
-      return this.currentTab;
+      return this.currentTab
     },
   },
 }
